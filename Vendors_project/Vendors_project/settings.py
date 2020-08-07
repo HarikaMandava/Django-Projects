21827f4ff82b1aff899bb35647dbbac0de3db7c1
HarikaMandava <harika.mand@gmail.com>
2020-08-07 11:07:39 +0530

    Web Application to display vendor details

--- Vendors_project/Vendors_project/settings.py
+++ Vendors_project/Vendors_project/settings.py
@@ -77,11 +77,8 @@ WSGI_APPLICATION = 'Vendors_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER':'postgres',
-        'PASSWORD': 'Hsuresh@6464',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
